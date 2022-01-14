ac07b6a3384b7de20b603b1c61c5b32bbaebdb2b
Zzz <zkmd2012@inbox.ru>
2022-01-14 15:53:51 +0100

    the use example of 'take' saga effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 
 
 function App() {
   const store = useSelector(store => store)
+  const dispatch = useDispatch()
   console.log(store)
   return (
     <div className="App">
       redux-saga
+      <button
+        onClick={() => dispatch({type: 'CLICK'})}
+      >
+        Click
+      </button>
     </div>
   );
 }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,3 +1,18 @@
+import {take} from 'redux-saga/effects'
+
+//take - указывает middleware ждать выполнения какого либо действия. По сути ждет какого либо диспатча в нашем приложении
+
+//for business actions. API requests, requests to cache, and other async actions
+export function* workerSaga() {
+
+}
+
+export function* watchClickSaga() {
+    yield take('CLICK')                //CLICK is a type of the action that we will dispatch
+
+    console.log('click from saga')
+}
+
 export default function* rootSaga() {
-    console.log('Saga!!!')
+    yield watchClickSaga()
 }
